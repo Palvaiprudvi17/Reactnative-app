273ad0c91de7bb81080fca90a76842542df613a9
Palvai Prudvi <133834261+Palvaiprudvi17@users.noreply.github.com>
2023-11-11 14:42:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Appnavigation from './SRC/Navigation/Appnavigation'
+
+const App = () => {
+  return (
+ <View>
+  <Text>this is arjuncheye</Text>
+ </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
